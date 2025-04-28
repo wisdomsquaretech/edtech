0aad8f0c901b5d1e9ad5de27ace5b34507da6629
apple <apple@apples-MacBook-Pro.local>
2025-04-28 12:39:24 +0530

    update in validation for user resource and api

--- backend/app/Filament/Resources/UserResource/Api/Requests/UpdateUserRequest.php
+++ backend/app/Filament/Resources/UserResource/Api/Requests/UpdateUserRequest.php
@@ -23,8 +23,8 @@ class UpdateUserRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'name' => 'required|string',
-            'email' => 'required|email',
+            'name' => 'required|string|min:2|max:255',
+            'email' => 'required|email|unique:users,email',
             'password' => 'required|string|min:8|confirmed',
             'role' => ['required', 'string', Rule::exists('roles', 'name')],
         ];
