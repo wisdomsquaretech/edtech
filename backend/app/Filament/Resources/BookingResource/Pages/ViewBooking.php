0e618996c2b12c1f671fa551f34855fa7b6b1675
apple <apple@apples-MacBook-Pro.local>
2025-06-11 14:49:43 +0530

    Update in booking resource for add tutor and lesson filter

---
+++ backend/app/Filament/Resources/BookingResource/Pages/ViewBooking.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Filament\Resources\BookingResource\Pages;
+
+use App\Filament\Resources\BookingResource;
+use Filament\Forms\Components\Placeholder;
+use Filament\Resources\Pages\ViewRecord;
+
+class ViewBooking extends ViewRecord
+{
+    protected static string $resource = BookingResource::class;
+}
